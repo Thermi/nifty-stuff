d2279c859fabdec951e33588f251113f8b619044
Noel Kuntze <noel@familie-kuntze.de>
2018-05-16 22:26:29 +0200

    Renamed notify_done stuff to notify-done

--- notify-done.sh
+++ notify-done.sh
@@ -1,9 +1,9 @@
 #! /bin/bash
 
 # Purpose is to run that after some long task to tell users that it's done.
-# E.g. xz -9 giantfile ; notify_done
+# E.g. xz -9 giantfile ; notify-done
 # paplay needs pulseaudio.
 
-function notify_done {
+function notify-done {
     paplay /usr/share/sounds/freedesktop/stereo/complete.oga
 }
